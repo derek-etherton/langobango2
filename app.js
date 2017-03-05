6a601bdfef42496e25b501b9167563c12d0cb3c6
derek-etherton <derek.etherton@mail.utoronto.ca>
2017-03-04 21:53:17 -0500

    Updated app port

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.use(function (req, res, next){
     return next();
 });
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(3000, function () {
   console.log('App listening on port 3000')
 });
