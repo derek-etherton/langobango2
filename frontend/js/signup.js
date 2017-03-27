b03466fa1bf2ea5f7e36ac7ab5ebca7391e0c269
AlexCav <alex.cavanagh@mail.utoronto.ca>
2017-03-26 22:13:02 -0400

    Fixed Password Confirm

--- frontend/js/signup.js
+++ frontend/js/signup.js
@@ -5,16 +5,22 @@
 		e.preventDefault();
 		var username = document.getElementById("signup_username").value;
 		var password = document.getElementById("signup_password").value;
+		var passConfirm = document.getElementById("signup_pass_confirm").value;
 		var email = document.getElementById("signup_email").value;
-		model.signUp({"username": username, "password": password, "email" : email}, function(err, res) {
-			if (err) {
-				alert ("Failed to create account");
-			}
-			if (res) {
-				window.location.replace('index.html');
-				alert("Account created");
-			}
-		});
+		if (password == passConfirm){
+			model.signUp({"username": username, "password": password, "email" : email}, function(err, res) {
+				if (err) {
+					alert ("Failed to create account");
+				}
+				if (res) {
+					window.location.replace('index.html');
+					alert("Account created");
+				}
+			});
+		}else{
+			alert("Passwords do not match!");
+		}
+
 	}
 
 }());
